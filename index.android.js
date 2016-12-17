57cbadf45ceb0bd3aeb8058bb939a021ad0a5e3a
Alberto Pedron <pedron.albert@gmail.com>
2016-12-17 09:41:40 -0400

    Add Router

--- index.android.js
+++ index.android.js
@@ -3,17 +3,19 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+import {Scene, Router} from 'react-native-router-flux'
 import React, { Component } from 'react'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 import { AppRegistry } from 'react-native'
 import Immutable from 'immutable'
-import SearchView from './app/views/SearchView'
 import lolcenaApp from './app/redux/reducers'
 import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
 import promiseMiddleware from 'redux-promise-middleware'
+
+// -------- Views --------------
+import SearchView from './app/views/SearchView'
 
 const logger = createLogger({
   stateTransformer: (state) => {
@@ -38,7 +40,11 @@ console.disableYellowBox = true
 export default class lolcena extends Component {
   render () {
     return <Provider store={store}>
-      <SearchView />
+      <Router>
+        <Scene key="root">
+          <Scene key="search_view" component={SearchView} hideNavBar={true} initial={true}/>
+        </Scene>
+      </Router>
     </Provider>
   }
 }
