40c16f9dad843d0ca6070572cd7904b4978945f4
Alberto Pedron <pedron.albert@gmail.com>
2016-12-18 07:25:00 -0400

    Add ScrollableTabs to SummonerProfile

--- app/views/SummonerProfileView/SummonerProfileView.js
+++ app/views/SummonerProfileView/SummonerProfileView.js
@@ -1,9 +1,10 @@
 import React, { Component, PropTypes } from 'react'
-import {View} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import {connect} from 'react-redux'
 import {Actions} from 'react-native-router-flux'
 import SummonerProfileViewToolbar from './SummonerProfileViewToolbar'
 import SummonerProfileViewActions from '../../redux/actions/SummonerProfileViewActions'
+import ScrollableTabView, {ScrollableTabBar} from 'react-native-scrollable-tab-view'
 
 class SummonerProfileView extends Component {
   componentWillMount () {
@@ -13,16 +14,32 @@ class SummonerProfileView extends Component {
   render () {
     let {summonerData} = this.props
     let handleOnPressBackButton = this.handleOnPressBackButton.bind(this)
+    let handleOnChangeTab = this.handleOnChangeTab.bind(this)
 
-    return <View>
+    return <View style={styles.root}>
       <SummonerProfileViewToolbar
         summonerData={summonerData}
         onPressBackButton={handleOnPressBackButton} />
+      <ScrollableTabView
+        initialPage={0}
+        renderTabBar={() => <ScrollableTabBar />}
+        onChangeTab={handleOnChangeTab}
+      >
+        <Text tabLabel="Champions" />
+        <Text tabLabel="League" />
+        <Text tabLabel="History" />
+        <Text tabLabel="Runes" />
+        <Text tabLabel="Masteries" />
+      </ScrollableTabView>
     </View>
   }
 
   handleOnPressBackButton () {
     Actions.pop()
+  }
+
+  handleOnChangeTab ({i: tabNumber}) {
+    console.log(tabNumber)
   }
 }
 
@@ -46,6 +63,12 @@ let mapDispatchToProps = (dispatch) => {
     }
   }
 }
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1
+  }
+})
 
 export default connect(
   mapStateToProps,
