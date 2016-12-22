bc1a093faa3bc63d089b3f4ff343f512f1f58f6f
Alberto Pedron <pedron.albert@gmail.com>
2016-12-22 14:54:46 -0400

    Responsive summoner profile toolbar

--- app/views/SummonerProfileView/SummonerProfileViewToolbar.js
+++ app/views/SummonerProfileView/SummonerProfileViewToolbar.js
@@ -1,75 +1,120 @@
 import React, { Component, PropTypes } from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
+import { View, Image, Text } from 'react-native';
+import { MediaQueryStyleSheet } from 'react-native-responsive';
 import IconButton from '../../components/IconButton';
 import LoadingScreen from '../../components/LoadingScreen';
 import regionHumanize from '../../utils/regionHumanize';
 
-const styles = StyleSheet.create({
-  backgroundImage: {
-    width: null,
-    height: null,
-    paddingBottom: 16,
-  },
-
-  summonerImage: {
-    width: 80,
-    height: 80,
-    borderRadius: 50,
-    borderWidth: 4,
-    borderColor: '#000',
-    marginBottom: 5,
-  },
-
-  summonerImageContainer: {
-    width: 80,
-    height: 80,
-    position: 'relative',
-    alignSelf: 'center',
-  },
-
-  summonerLevelContainer: {
-    position: 'absolute',
-    width: 25,
-    height: 25,
-    backgroundColor: 'black',
-    bottom: 0,
-    right: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 50,
-  },
-
-  summonerLevelText: {
-    color: 'white',
-    textAlign: 'center',
-  },
-
-  summonerNameText: {
-    fontSize: 16,
-    color: '#FFF',
-    textShadowColor: '#000',
-    textShadowOffset: {
-      width: 1.5,
-      height: 1.5,
+const styles = MediaQueryStyleSheet.create(
+  {
+    backgroundImage: {
+      width: null,
+      height: null,
+      paddingBottom: 16,
     },
-    textAlign: 'center',
-  },
 
-  regionText: {
-    color: '#FFF',
-    textAlign: 'center',
-    textShadowColor: '#000',
-    textShadowOffset: {
-      width: 1.5,
-      height: 1.5,
+    summonerImage: {
+      width: 80,
+      height: 80,
+      borderRadius: 50,
+      borderWidth: 4,
+      borderColor: '#000',
+      marginBottom: 5,
+    },
+
+    summonerImageContainer: {
+      width: 80,
+      height: 80,
+      position: 'relative',
+      alignSelf: 'center',
+    },
+
+    summonerLevelContainer: {
+      position: 'absolute',
+      width: 25,
+      height: 25,
+      backgroundColor: 'black',
+      bottom: 0,
+      right: 0,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 50,
+    },
+
+    summonerLevelText: {
+      color: 'white',
+      textAlign: 'center',
+    },
+
+    summonerNameText: {
+      fontSize: 16,
+      color: '#FFF',
+      textShadowColor: '#000',
+      textShadowOffset: {
+        width: 2,
+        height: 2,
+      },
+      textAlign: 'center',
+    },
+
+    regionText: {
+      color: '#FFF',
+      textAlign: 'center',
+      textShadowColor: '#000',
+      textShadowOffset: {
+        width: 1.5,
+        height: 1.5,
+      },
+    },
+
+    profileToolbar: {
+      marginTop: -30,
+      height: 125,
     },
   },
+  {
+    '@media (min-device-width: 600)': {
+      profileToolbar: {
+        marginTop: 0,
+      },
 
-  profileToolbar: {
-    marginTop: -30,
-    height: 125,
+      profileToolbarContainer: {
+        flexDirection: 'row',
+        marginLeft: 70,
+        marginRight: 40,
+      },
+
+      summonerImageContainer: {
+        width: 100,
+        height: 100,
+      },
+
+      summonerImage: {
+        width: 100,
+        height: 100,
+      },
+
+      summonerLevelContainer: {
+        width: 30,
+        height: 30,
+      },
+
+      summonerDataContainer: {
+        flex: 1,
+        marginLeft: 40,
+        justifyContent: 'center',
+      },
+      summonerNameText: {
+        fontSize: 30,
+        textAlign: 'left',
+      },
+      regionText: {
+        fontSize: 21,
+        textAlign: 'left',
+      },
+    },
   },
-});
+);
 
 class SummonerProfileViewToolbar extends Component {
   // TODO: Imagenes desde el servidor
@@ -102,7 +147,7 @@ class SummonerProfileViewToolbar extends Component {
         {isFetching ? (
           <LoadingScreen />
         ) : (
-          <View>
+          <View style={styles.profileToolbarContainer}>
             <View style={styles.summonerImageContainer}>
               <Image
                 style={styles.summonerImage}
@@ -113,8 +158,10 @@ class SummonerProfileViewToolbar extends Component {
               </View>
             </View>
 
-            <Text style={styles.summonerNameText}>{name}</Text>
-            <Text style={styles.regionText}>{regionHumanize(region)}</Text>
+            <View style={styles.summonerDataContainer}>
+              <Text style={styles.summonerNameText}>{name}</Text>
+              <Text style={styles.regionText}>{regionHumanize(region)}</Text>
+            </View>
           </View>
         )}
       </View>
