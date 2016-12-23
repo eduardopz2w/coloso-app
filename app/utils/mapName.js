5ae4849057b37eba7dbc6affa8ccc6f1208be300
Alberto Pedron <pedron.albert@gmail.com>
2016-12-23 10:46:06 -0400

    Add all maps

--- app/utils/mapName.js
+++ app/utils/mapName.js
@@ -1,7 +1,12 @@
-// TODO: Agregar todos los mapas
 export default function (mapId) {
   if (mapId === 1 || mapId === 2 || mapId === 11) {
     return 'Grieta del invocador';
+  } if (mapId === 4 || mapId === 10) {
+    return 'Bosque retorcido';
+  } if (mapId === 8) {
+    return 'Cicatríz de cristal';
+  } if (mapId === 3 || mapId === 12 || mapId === 14) {
+    return 'Abismo de los lamentos';
   }
 
   return mapId;
