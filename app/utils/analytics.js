3a5e03ace0b25c81a482f8ab930f403bbcc58267
Alberto Pedron <pedron.albert@gmail.com>
2016-12-28 16:10:57 -0400

    Tracking Only on Prod

--- app/utils/analytics.js
+++ app/utils/analytics.js
@@ -2,4 +2,22 @@ import { GoogleAnalyticsTracker } from 'react-native-google-analytics-bridge';
 
 const TRACK_ID = 'UA-59658116-4';
 
-export const tracker = new GoogleAnalyticsTracker(TRACK_ID);
+export const gaTracker = new GoogleAnalyticsTracker(TRACK_ID);
+
+export const tracker = {
+  trackScreenView(screenName) {
+    if (!__DEV__) {
+      gaTracker.trackScreenView(screenName);
+    } else {
+      console.log(`Tracking Screen: ${screenName}`);
+    }
+  },
+
+  trackEvent(category, action, optionalValues) {
+    if (!__DEV__) {
+      gaTracker.trackEvent(category, action, optionalValues);
+    } else {
+      console.log(`Tracking Event categoery: ${category}, action: ${action}`);
+    }
+  },
+};
