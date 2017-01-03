4b5b966daba7e7fe76673098b7a13a35b5317d48
Alberto Pedron <pedron.albert@gmail.com>
2017-01-03 03:56:35 -0400

    Fix proptype warning

--- app/components/Drawer.js
+++ app/components/Drawer.js
@@ -7,9 +7,17 @@ class MainDrawer extends PureComponent {
   render() {
     const state = this.props.navigationState;
     const children = state.children;
+    const styles = {};
+
+    if (state.open) {
+      styles.mainOverlay = {
+        backgroundColor: 'rgba(0,0,0,0.4)',
+      };
+    }
+
     return (<Drawer
       open={state.open}
-      styles={state.open && { mainOverlay: { backgroundColor: 'rgba(0,0,0,0.4)' } }}
+      styles={styles}
       onOpen={() => Actions.refresh({ key: state.key, open: true })}
       onClose={() => Actions.refresh({ key: state.key, open: false })}
       type="overlay"
