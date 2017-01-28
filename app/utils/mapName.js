31eca1b3ddf90d2c342b457bb6d325e6f54a31d8
Alberto Pedron <pedron.albert@gmail.com>
2017-01-28 05:16:45 -0400

    Add queue translations and fix mapName translations

--- app/utils/mapName.js
+++ app/utils/mapName.js
@@ -2,13 +2,13 @@ import I18n from 'i18n-js';
 
 export default function (mapId) {
   if (mapId === 1 || mapId === 2 || mapId === 11) {
-    return I18n.t('summoner_rift');
+    return I18n.t('maps.summoner_rift');
   } if (mapId === 4 || mapId === 10) {
-    return I18n.t('twisted_treeline');
+    return I18n.t('maps.twisted_treeline');
   } if (mapId === 8) {
-    return I18n.t('crystal_scar');
+    return I18n.t('maps.crystal_scar');
   } if (mapId === 3 || mapId === 12 || mapId === 14) {
-    return I18n.t('howling_abyss');
+    return I18n.t('maps.howling_abyss');
   }
 
   return mapId;
