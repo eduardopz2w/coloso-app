17f362e341449729ffdef368be89ff7d2877cbd4
Alberto Pedron <pedron.albert@gmail.com>
2017-01-27 20:27:51 -0400

    Fix storage warnings

--- app/redux/actions/OwnerAccountActions.js
+++ app/redux/actions/OwnerAccountActions.js
@@ -1,6 +1,13 @@
 import { createAction } from 'redux-actions';
+import logger from '../../utils/logger';
 
-export const loadAccount = createAction('OWNER_ACCOUNT/LOAD_ACCOUNT', () => Storage.load({ key: 'ownerAccount' }));
+export const loadAccount = createAction('OWNER_ACCOUNT/LOAD_ACCOUNT', () => new Promise((resolve) => {
+  Storage.load({ key: 'ownerAccount' })
+    .then(resolve)
+    .catch(() => {
+      logger.debug('Can not load ownerAccount from storeage');
+    });
+}));
 export const saveAccount = createAction('OWNER_ACCOUNT/SAVE_ACCOUNT', (ownerAccount) => {
   Storage.save({ key: 'ownerAccount', rawData: ownerAccount });
 
